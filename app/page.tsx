1e37e9484a89059deb0caa35bada4024ef4da162
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2026-01-30 23:43:08 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-500/50 font-mono">
+        Hello World
+      </h1>
+    </div>
+  );
+}
